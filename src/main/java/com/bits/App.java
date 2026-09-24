3db17289c0f80604d4a064f74ef2921cad7f7cc9
2022wb86912 <2022wb86912@wilp.bits-pilani.ac.in>
2026-09-24 21:11:25 +0530

    Update output message in App.java

--- src/main/java/com/bits/App.java
+++ src/main/java/com/bits/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Continuous Integration using Jenkins");
+        System.out.println("Continuous Integration using Jenkins by Kaustav");
     }
 }
